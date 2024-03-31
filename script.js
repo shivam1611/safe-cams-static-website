b8aee2183003bd99ed1c928badda6ae9ae5f519c
codeshey <99895966+shivam1611@users.noreply.github.com>
2024-03-31 16:15:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function navFunction(){
+    let nav = document.getElementById("navlist");
+    if(nav.style.display == "none")
+    {
+        nav.style.display = "block";
+    }
+    else{
+        nav.style.display = "none";
+    }
+
+}
